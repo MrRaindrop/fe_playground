a4680398315c5928f2b7f9531d3bcf416e805813
Mr.Raindrop <tekkahs@gmail.com>
2015-02-21 21:25:44 +0800

    update

--- demo/promise/promise_test/test.js
+++ demo/promise/promise_test/test.js
@@ -10,23 +10,23 @@ describe('Array', function(){
     console.log('---- before all tests ----');
   });
 
-	beforeEach(function() {
-		console.log('---- before every function ----');
-	});
+  beforeEach(function() {
+    console.log('---- before every function ----');
+  });
 
-	afterEach(function() {
-		console.log('---- after every function ----');
-	});
-
+  afterEach(function() {
+    console.log('---- after every function ----');
+  });
+  
   describe('#indexOf()', function(){
 
-  	beforeEach(function() {
-			console.log('---- before every #indexOf() function ----');
-		});
+    beforeEach(function() {
+      console.log('---- before every #indexOf() function ----');
+    });
 
-		afterEach(function() {
-			console.log('---- after every #indexOf() function ----');
-		});
+    afterEach(function() {
+      console.log('---- after every #indexOf() function ----');
+    });
 
     it('should return -1 when the value is not present.', function(){
       assert.equal(-1, [1,2,3].indexOf(5));
@@ -35,9 +35,9 @@ describe('Array', function(){
   });
 
   it('should compare 0 and false', function() {
-		assert.equal(0, false);
-	});
-
+    assert.equal(0, false);
+  });
+  
   describe('1equalTrue()', function(){
     it('1 should be equal to true.', function(){
       assert.equal(1, true);
@@ -45,7 +45,7 @@ describe('Array', function(){
   });
 
   after(function() {
-  	console.log('---- after all tests ----');
+    console.log('---- after all tests ----');
   });
 
 });
