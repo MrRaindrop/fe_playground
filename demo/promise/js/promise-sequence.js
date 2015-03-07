0df771a4f851dac3f194d5111e77b8c2306fe095
Mr.Raindrop <tekkahs@gmail.com>
2015-03-07 21:57:15 +0800

    refactor - extract function

--- demo/promise/js/promise-sequence.js
+++ demo/promise/js/promise-sequence.js
@@ -9,56 +9,62 @@
 					resolve('timer ' + time + 's');
 				}, _t);
 			});
-		};
+		},
+
+    sequencePromises = function(promises) {
+
+    	var results = [],
+
+        handleResult = function(val) {
+          if (promises.length === 0) {
+            console.log(Date.now() - startDate + 'ms');
+            console.log(results);
+          } else {
+            results.push(val);
+            promises.shift();
+            console.log(Date.now() - startDate + 'ms');
+            console.log(results);
+          }
+        },
+
+        startDate = Date.now();
+
+	    /**
+	     * 直接使用then来链接所有promise使之序列执行
+	     */
+			// getPromisedTimer(1).then(handleResult)
+			// 	.then(getPromisedTimer.bind(null, 2)).then(handleResult)
+			// 	.then(getPromisedTimer.bind(null, 4)).then(handleResult)
+			// 	.then(getPromisedTimer.bind(null, 8)).then(handleResult);
+
+	    /**
+	     * 使用for循环来处理promise的序列执行
+	     */
+			// var promise = promises[0]().then(handleResult);
+			// for (var i = 1, l = promises.length; i < l; i++) {
+			// 	promise = promise.then(promises[i]).then(handleResult);
+			// }
+	    
+	    /**
+	     * 使用reduce处理promise的序列执行（比for循环更聪明的用法）
+	     */
+	    
+	    promises.reduce(function(prevPromise, currPromise) {
+	      return prevPromise.then(currPromise).then(handleResult);
+	    }, promises[0]().then(handleResult));
+
+    };
 
 	btnSequence.addEventListener('click', function(e) {
 
-		var startDate,
-			results = [],
-			promises = [
+		var promises = [
 			  getPromisedTimer.bind(null, 1),
 				getPromisedTimer.bind(null, 2),
 				getPromisedTimer.bind(null, 4),
 				getPromisedTimer.bind(null, 8)
-			],
+			];
 
-			handleResult = function(val) {
-				if (promises.length === 0) {
-					console.log(Date.now() - startDate + 'ms');
-					console.log(results);
-				} else {
-					results.push(val);
-					promises.shift();
-					console.log(Date.now() - startDate + 'ms');
-					console.log(results);
-				}
-			};
-
-		startDate = Date.now();
-
-    /**
-     * 直接使用then来链接所有promise使之序列执行
-     */
-		// getPromisedTimer(1).then(handleResult)
-		// 	.then(getPromisedTimer.bind(null, 2)).then(handleResult)
-		// 	.then(getPromisedTimer.bind(null, 4)).then(handleResult)
-		// 	.then(getPromisedTimer.bind(null, 8)).then(handleResult);
-
-    /**
-     * 使用for循环来处理promise的序列执行
-     */
-		// var promise = promises[0]().then(handleResult);
-		// for (var i = 1, l = promises.length; i < l; i++) {
-		// 	promise = promise.then(promises[i]).then(handleResult);
-		// }
-    
-    /**
-     * 使用reduce处理promise的序列执行（比for循环更聪明的用法）
-     */
-    
-    promises.reduce(function(prevPromise, currPromise) {
-      return prevPromise.then(currPromise).then(handleResult);
-    }, promises[0]().then(handleResult));
+			sequencePromises(promises);
 
 	}, false);
 
